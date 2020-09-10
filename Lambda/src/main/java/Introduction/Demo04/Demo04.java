17f38881ce15263836c696fa1a4ad93b53e918dd
Qing <1772344524@qq.com>
2020-09-10 16:43:34 +0800

    stream的map方法

--- Lambda/src/main/java/Introduction/Demo04/Demo04.java
+++ Lambda/src/main/java/Introduction/Demo04/Demo04.java
@@ -31,7 +31,7 @@ public class Demo04 {
          * Todo-comment Lambda-04: Lambda表达式的第二种写法：省略参数 采用 `类/对象 :: 方法名`
          *
          *    第二种：map的参数是一个函数式接口 Function
-         *            Function接口内部有一个抽象方法accept
+         *            Function接口内部有一个抽象方法`apply`
          *                这里`String::toUpperCase` 即：对该函数式接口中的accept方法的具体实现
          *
          *                  //即：第二种...的原始写法...
@@ -43,6 +43,14 @@ public class Demo04 {
          *                  });
          */
         //3.采用Stream 流
+        /**
+         * Todo-comment Lambda-05: 关于 stream的`map方法`
+         *
+         *     map方法中的函数式接口Function
+         *         Function中的`apply方法` 接收参数 **返回结果**
+         *             返回结果 作为 `map方法的具体实现` 中 `downstream.accept`方法的参数
+         *                 **返回结果是一个Stream**
+         */
         list.stream().map(item -> item.toUpperCase()).forEach(item -> System.out.println(item));
         list.stream().map(String::toUpperCase).forEach(item -> System.out.println(item));
     }
