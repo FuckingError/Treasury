be6b70e8a4771be4caee94679c2c1af533ffacbc
Qing <1772344524@qq.com>
2020-09-16 16:15:42 +0800

    Function compose 和 andThen

--- Lambda/src/main/java/_02_FunctionDoc/FunctionTest.java
+++ Lambda/src/main/java/_02_FunctionDoc/FunctionTest.java
@@ -8,7 +8,13 @@ import java.util.function.Function;
  */
 public class FunctionTest{
     public static void main(String[] args) {
+        FunctionTest test = new FunctionTest();
 
+        System.out.println("自增：" + test.compute(1, value -> ++value));
+
+        System.out.println("乘2：" + test.compute(2,value->value*2));
+
+        System.out.println("大写转小写"+test.convert("ABC",String :: toLowerCase));
     }
 
     /*
@@ -16,6 +22,10 @@ public class FunctionTest{
     * */
     public int compute(int a,Function<Integer,Integer> function){
         return function.apply(a);
+    }
+
+    public String convert(String s,Function<String,String> function){
+        return function.apply(s);
     }
 
 
---
+++ Lambda/src/main/java/_02_FunctionDoc/FunctionTest2.java
@@ -0,0 +1,27 @@
+package _02_FunctionDoc;
+
+import java.util.function.Function;
+
+/**
+ * @author Qing
+ * @version 1.0
+ * @date 2020/9/16 下午 1:32
+ */
+public class FunctionTest2 {
+    public static void main(String[] args) {
+        /**
+         * Todo-comment lambda-08: Function compose 和 andThen
+         */
+        FunctionTest2 test = new FunctionTest2();
+        System.out.println(test.compute(2,value -> value * 3,value -> value * value));
+        System.out.println(test.compute2(2,value -> value * 3,value -> value * value));
+    }
+
+    public int compute(int a, Function<Integer,Integer> function1,Function<Integer,Integer> function2){
+        return function1.compose(function2).apply(a);
+    }
+
+    public int compute2(int a, Function<Integer,Integer> function1,Function<Integer,Integer> function2){
+        return function1.andThen(function2).apply(a);
+    }
+}
