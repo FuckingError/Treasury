308e88c15e63863c011c2b0a5a8bb1e430b8ee92
Qing <1772344524@qq.com>
2020-09-16 10:41:36 +0800

    lambda表达式定义和作用

--- Lambda/src/main/java/_02_FunctionDoc/Demo02.java
+++ Lambda/src/main/java/_02_FunctionDoc/Demo02.java
@@ -12,13 +12,23 @@ public class Demo02 {
         List<String> letters = Arrays.asList("b", "c", "a", "e");
         //逆置
         //Todo-Collect 01 : sort 按照 compare的结果 >0
-      /*  Collections.sort(letters, new Comparator<String>() {
+        Collections.sort(letters, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 return o2.compareTo(o1);
             }
-        });*/
+        });
 
+        /**
+         * Todo-comment lambda-07: Lambda表达式总结
+         *     - Lambda表达式是一种匿名函数
+         *         - 没有声明：没有访问修饰符、返回值、名字
+         *     - 作用
+         *         - 传递**行为** 而不仅仅是值
+         *             - 提升抽象层次
+         *             - API重用性更好
+         *             - 更加灵活
+         */
         Collections.sort(letters,(o1,o2) -> o2.compareTo(o1));
 
         System.out.println(letters);
